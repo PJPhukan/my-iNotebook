83903129a04e8db51ae235de7d93afce18a8cd15
PJPhukan <paragjyotiphukan2004@gmail.com>
2024-02-15 14:05:22 +0530

    Edit Some functionality

--- src/Components/Home/Note/NoteItem/Noteitem.jsx
+++ src/Components/Home/Note/NoteItem/Noteitem.jsx
@@ -5,14 +5,14 @@ import "./Noteitem.css"
 export default function Noteitem(props) {
     const context = useContext(noteContext);
     const { deleteNotes } = context;
-    const {note}=props;
+    const { note, upDateNotes }=props;
     return (
         <div className='col-md-3 my-3'>
             <div className="card my-3" style={{"width": "15rem"}}>
                 <div className="card-body">
                     <h5 className="card-title">{note.title}</h5>
                     <p className="card-text">{note.description}</p>
-                    <i className="fa-solid fa-pen-to-square mx-2" style={{ "color": "green" }} />
+                    <i className="fa-solid fa-pen-to-square mx-2" style={{ "color": "green" }} onClick={() => { upDateNotes(note)}}/>
                     <i className="fa-solid fa-trash mx-2" style={{ "color": "red" }} onClick={() => { deleteNotes(note._id) }} />
                 </div>
             </div>
--- src/Components/Home/Note/Notes.jsx
+++ src/Components/Home/Note/Notes.jsx
@@ -1,22 +1,61 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useRef,useState } from 'react'
 import noteContext from '../../../Context/notes/noteContext'
 import Noteitem from './NoteItem/Noteitem';
 import AddNote from './AddNote';
 export default function Notes() {
   const context = useContext(noteContext);
   const { notes, getNotes } = context;
+  const ref = useRef(null)
+  const upDateNotes = () => {
+    ref.current.click();
+  }
+  const [note, setNote] = useState({ title: "", description: "", tag: "default" })
+
   useEffect(() => {
     getNotes();
-  
+    // eslint-disable-next-line
   }, [])
-  
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    // addNotes(note.title, note.description, note.tag);
+  }
+
+  const onchange = (e) => {
+    setNote({ ...note, [e.target.name]: e.target.value })
+  }
   return (
     <>
-    <AddNote/>
+      <AddNote />
+      <button type="button" class="btn btn-primary" style={{ display:'none' }} data-bs-toggle="modal" data-bs-target="#exampleModal" ref={ref}>Clidk me
+      </button>
+      <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div className="modal-body">
+              <label htmlFor="title" className="form-label">Title</label>
+              <input type="text" className="form-control" id="etitle" name='etitle' placeholder="xyz_title" onChange={onchange} />
+              <label htmlFor="description" className="form-label my-3" >Description</label>
+              <input type="text" id="edescription" name='edescription' className="form-control" aria-describedby="passwordHelpBlock" onChange={onchange} />
+              <label htmlFor="tag" className="form-label my-3" >Tag</label>
+              <input type="text" id="etag" name='etag' className="form-control" aria-describedby="passwordHelpBlock" onChange={onchange} />
+              <input className="btn btn-primary my-3" type="submit" value="Add Note" onClick={handleClick} />
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              <button type="button" className="btn btn-primary">Update Notes</button>
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="row my-3">
         <h1>Your Notes</h1>
         {notes.map((note) => {
-          return <Noteitem key={note._id} note={note} />
+          return <Noteitem key={note._id} upDateNotes={upDateNotes} note={note} />
         })}
       </div>
     </>
