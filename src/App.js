2ed91dc7b493ff04a2c0edf5fbd762cc5d419a10
PJPhukan <paragjyotiphukan2004@gmail.com>
2024-03-02 11:21:18 +0530

    add login functionality

--- src/App.js
+++ src/App.js
@@ -8,22 +8,26 @@ import Navber from './Components/Home/Navber/Navber'
 import About from './Components/About/About'
 import NoteState from './Context/notes/noteState';
 import Alert from './Components/Home/Note/Alert/Alert';
+import Login from './Components/Authentication/Login';
+import Signup from './Components/Authentication/Signup';
 function App() {
   return (
     <>
-    {/* Context api */}
+      {/* Context api */}
       <NoteState>
         {/* Router setup */}
         <BrowserRouter>
           <Navber />
-          <Alert massage="This is an alert"/>
+          <Alert massage="This is an alert" />
           <div className="container">
-          <Routes>
-            <Route exact path='/' element={<Home />} />
-            <Route exact path='/about' element={<About />} />
-          </Routes>
+            <Routes>
+              <Route exact path='/' element={<Home />} />
+              <Route exact path='/about' element={<About />} />
+              <Route exact path='/login' element={<Login />} />
+              <Route exact path='/signup' element={<Signup />} />
+            </Routes>
           </div>
-            
+
         </BrowserRouter>
       </NoteState>
 
---
+++ src/Components/Authentication/Login.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const host = "http://localhost:8000";
+
+const Login = () => {
+    const [login, setlogin] = useState({ email: "", password: "" })
+    let navigate = useNavigate();
+
+
+    const onchange = (e) => {
+        setlogin({ ...login, [e.target.name]: e.target.value })
+    }
+
+
+    const HandleSubmit = async (e) => {
+        e.preventDefault();
+
+        //API CALL(backend)
+        const url = `${host}/api/auth/login`
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ email: login.email, password: login.password })
+        });
+        const json = await response.json();
+        console.log(json)
+        if (json.success) {
+            //save the auth token and redirect
+            localStorage.setItem("token", json.authToken);
+            navigate('/');
+        }
+        else {
+            if (json.error) {
+                alert(`Login failed: ${json.error}`);
+            } else {
+                alert("Login failed. Please try again.");
+            }
+        }
+    }
+
+    return (
+        <div className='container'>
+            <form onSubmit={HandleSubmit}>
+                <div className="mb-3">
+                    <label htmlFor="email" className="form-label">Email address</label>
+                    <input type="email" className="form-control" id="email" name='email' aria-describedby="emailHelp" onChange={onchange} value={login.email} />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="password" className="form-label">Password</label>
+                    <input type="password" className="form-control" id="password" name='password' onChange={onchange} value={login.password} />
+                </div>
+
+                <button className="btn btn-primary" type="submit">Login</button>
+            </form>
+        </div>
+    )
+}
+
+export default Login
